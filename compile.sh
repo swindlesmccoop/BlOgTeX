5f904663b4da060cb7ce59d62bb0dacf29caac6e
swindlesmccoop <swindlesmccoop@gmail.com>
2023-05-08 17:41:16 +0000

    Change to using pandock isntead tex4ht

--- compile.sh
+++ compile.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 
-#paths can be relative or absolute
-SOURCE="/usr/share/blog-tex"
-OUTPUT="/usr/share/blog-html"
-#CSS="/css/blog.css"
+#paths must be absolute
+SOURCE="$HOME/workspace/blogtex/blog-tex"
+OUTPUT="$HOME/workspace/blogtex/blog-html"
+CSS="/css/blog.css"
 mkdir -p "$SOURCE" "$OUTPUT"
+
+#cleanup
+rm -f "$OUTPUT/*"
 
 #getting vars
 cd "$SOURCE"
-TEXFILES="$(find . -name "*.tex")"
+TEXFILES="$(ls -1 *.tex | sed 's/\.tex//g')"
 
 #INTENSE STREAM MANIPULATION
-egrep "date|title" -m 3 *.tex | sed -e 's/\\date{//; s/:/ /; s/}//; /maketitle/d' | paste - - | sed -e 's/^/<a href="/; s/ \\title{/">/; s/.*tex//2g; s/\.tex/\.html/' | awk '{for (i=1; i<=NF-2; i++) printf "%s ", $i; print $NF}' | sed -e 's/\(.*\) /\1<\/a> - /; s/$/<br>/' | awk '{for(i=NF;i>1;i--)printf "%s ",$i;printf "%s",$1;print ""}' | sort -rn -k 1.8 -k 1.7 -k 1.5 -k 1.4 -k 1.2 -k 1.1 | awk '{for(i=NF;i>1;i--)printf "%s ",$i;printf "%s",$1;print ""}' > files.html
+egrep "date|title" -m 3 *.tex | sed -e 's/\\date{//; s/:/ /; s/}//; /maketitle/d' | paste - - | sed -e 's/^/<a href="/; s/\\title{/">/; s/.*tex//2g; s/\.tex/\.html/' | awk '{for (i=1; i<=NF-2; i++) printf "%s ", $i; print $NF}' | sed -e 's/\(.*\) /\1<\/a> - /; s/$/<br>/' | awk '{for(i=NF;i>1;i--)printf "%s ",$i;printf "%s",$1;print ""}' | sort -rn -k 1.8 -k 1.7 -k 1.5 -k 1.4 -k 1.2 -k 1.1 | awk '{for(i=NF;i>1;i--)printf "%s ",$i;printf "%s",$1;print ""}' > $SOURCE/files.html
 
-for file in $TEXFILES; do printf "Compiling \033[0;32m$(htlatex "$file" | grep "tex4ht -f" | sed 's|tex4ht -f/./||')\033[0m \n"; done
-rm *.4ct *.4tc *.aux *.dvi *.idv *.lg *.log *.tmp *.xref
+for file in $TEXFILES; do
+	printf "Compiling \033[0;32m"$file".tex\033[0m\n"
+	pandoc "$file.tex" -o "$OUTPUT/$file.html" --css=$CSS
+done
 
-[ -f header.html ] || printf "Error: no header file present. Header files should contain everything for the blog index aside from the actual index of files.\n"
-cat header.html files.html > index.html && rm files.html
-printf '<link rel="stylesheet" href="/css/blog.css">' | tee -a *.html > /dev/null
-sed -i '$d' index.html header.html
-mv $SOURCE/*.html $SOURCE/*.css "$OUTPUT"
-mv "$OUTPUT/header.html" "$SOURCE"
+cat $SOURCE/header.html $SOURCE/files.html > $OUTPUT/index.html
+rm $SOURCE/files.html
